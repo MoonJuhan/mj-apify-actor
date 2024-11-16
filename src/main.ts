b19d91e2a0621e1f2b0528f55ba943029e7bf1c7
MoonJuhan <codemike@naver.com>
2024-11-16 17:03:58 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,5 @@
-/**
- * This template is a production ready boilerplate for developing with `PlaywrightCrawler`.
- * Use this to bootstrap your projects using the most up-to-date code.
- * If you're looking for examples or want to learn more, see README.
- */
-
-// For more information, see https://docs.apify.com/sdk/js
 import { Actor } from 'apify'
-// For more information, see https://crawlee.dev
-import { PlaywrightCrawler } from 'crawlee'
-// this is ESM project, and as such, it requires you to specify extensions in your relative imports
-// read more about this here: https://nodejs.org/docs/latest-v18.x/api/esm.html#mandatory-file-extensions
-// note that we need to use `.js` even when inside TS files
-import { router } from './routes.js'
+import { Dataset, PlaywrightCrawler } from 'crawlee'
 
 interface Input {
   startUrls: string[]
@@ -21,8 +9,10 @@ interface Input {
 // Initialize the Apify SDK
 await Actor.init()
 
+const targetUrl = process.env.APIFY_TARGET_URL
+
 // Structure of input is defined in input_schema.json
-const { startUrls = ['https://crawlee.dev'], maxRequestsPerCrawl = 100 } =
+const { startUrls = [targetUrl as string], maxRequestsPerCrawl = 100 } =
   (await Actor.getInput<Input>()) ?? ({} as Input)
 
 const proxyConfiguration = await Actor.createProxyConfiguration()
@@ -30,7 +20,23 @@ const proxyConfiguration = await Actor.createProxyConfiguration()
 const crawler = new PlaywrightCrawler({
   proxyConfiguration,
   maxRequestsPerCrawl,
-  requestHandler: router,
+  async requestHandler({ page, request, log }) {
+    log.info(`Crawling ${request.url}`)
+
+    const title = await page.title()
+    const html = await page.content()
+    log.info(`${title}`, { url: request.loadedUrl })
+
+    await Dataset.pushData({
+      url: request.loadedUrl,
+      title,
+      html,
+    })
+  },
+  // 오류 처리기
+  failedRequestHandler({ request, error }) {
+    console.log(`Request ${request.url} failed: ${error?.message}`)
+  },
 })
 
 await crawler.run(startUrls)
