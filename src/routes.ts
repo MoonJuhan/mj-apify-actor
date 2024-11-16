2557de787cbfb7478ee1c0b38631c8065d6b07e5
MoonJuhan <codemike@naver.com>
2024-11-16 16:41:38 +0900

    feat: Add html

--- src/routes.ts
+++ src/routes.ts
@@ -12,10 +12,12 @@ router.addDefaultHandler(async ({ enqueueLinks, log }) => {
 
 router.addHandler('detail', async ({ request, page, log }) => {
   const title = await page.title()
+  const html = await page.content()
   log.info(`${title}`, { url: request.loadedUrl })
 
   await Dataset.pushData({
     url: request.loadedUrl,
     title,
+    html,
   })
 })
