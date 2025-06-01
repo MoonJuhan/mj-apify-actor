216d157a6d49d26893acceee30830ed0d46b8ee0
MoonJuhan <codemike@naver.com>
2025-06-01 15:43:47 +0900

    fix

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,10 @@ import { createPlaywrightRouter, Dataset } from 'crawlee';
 
 export const router = createPlaywrightRouter();
 
+router.addDefaultHandler(async ({ request, log }) => {
+    log.info(`Default handler: ${request.url}`);
+});
+
 router.addHandler('detail', async ({ request, page, log }) => {
     log.info(`Crawling ${request.url}`);
     const title = await page.title();
